cf4e67ccb9333f05ad9fe33fa63d722f7a2123e1
Katchietsnouva <philipaswa01@gmail.com>
2024-01-13 02:05:19 +0300

    Merge remote changes 17

--- Assets/scripts/u6_slider_ctrl.cs
+++ Assets/scripts/u6_slider_ctrl.cs
@@ -8,10 +8,11 @@ public class u6_slider_ctrl : MonoBehaviour
     private Slider slider1;
     [SerializeField] private GameObject parentJointBox;
     [SerializeField] private GameObject childJointBox;
+    [SerializeField] private GameObject childJoint3Box;
     // Initial offset of the child joint in the x-axis
-    private float initialChildJointOffset = (float)(0.0754 / 100.0); 
+    private float initialChildJointOffset = (float)(0.0754 / 100.0);
     // Adjust this value based on your scene
-    private float parentJointRadius  = (float)(0.0754 / 100.0); 
+    private float parentJointRadius = (float)(0.0754 / 100.0);
 
     void Start()
     {
@@ -23,9 +24,23 @@ public class u6_slider_ctrl : MonoBehaviour
         AlterJointWithVariables(rotationValue1, parentJointBox, Vector3.zero);
 
         // Calculate offset position for the child joint
-        // Vector3 childJointOffsetPosition = Quaternion.Euler(0, rotationValue1, 0) * new Vector3(initialChildJointOffset-parentJointRadius, 0, 0);
         Vector3 childJointOffsetPosition = Quaternion.Euler(0, rotationValue1, 0) * new Vector3(-initialChildJointOffset, 0, 0) + new Vector3(parentJointRadius, 0, 0);
         AlterJointWithVariables(rotationValue1, childJointBox, childJointOffsetPosition);
+        // Calculate offset position for the child joint3    // Set the 2nd child as a child of the 1st child
+        
+        if (childJointBox != null && childJoint3Box != null)
+        {
+            childJoint3Box.transform.SetParent(childJointBox.transform);
+        }
+
+        // You can calculate offset position for the childJoint3Box if needed
+        // Vector3 childJoint3OffsetPosition = ...;
+
+        // Alter the rotation and position of the childJoint3Box
+        AlterJointWithVariables(rotationValue1, childJoint3Box, Vector3.zero);
+
+
+
     }
 
 
