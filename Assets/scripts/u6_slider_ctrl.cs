836d64035550e70dacd8109bfadd511951d0181b
Katchietsnouva <philipaswa01@gmail.com>
2024-02-25 17:32:16 +0300

    android try v2.9

--- Assets/scripts/u6_slider_ctrl.cs
+++ Assets/scripts/u6_slider_ctrl.cs
@@ -156,44 +156,7 @@ public class u6_slider_ctrl : MonoBehaviour
       // if (isServer && isNetworkingEnabled)
         // else if (!isServer && isNetworkingEnabled)
 
-    
     // public void ToggleClientServerMode(bool isNetworkingEnabled)
-    // {
-    //     isServer = !isServer; // Toggle server mode
-    //     Debug.Log("Pressed. isServer: " + isServer + ", isNetworkingEnabled: " + isNetworkingEnabled);
-
-    //     if (isServer)
-    //     {
-    //         // Implement logic to set the device as a server e.g., start listening for clients
-    //         Debug.Log("Server Mode");
-    //         if (isNetworkingEnabled)
-    //         {
-    //             Debug.Log("Networking Enabled. Setting up");
-    //             // StartServer();
-    //         }
-    //         else
-    //         {
-    //             Debug.Log("Networking is not enabled");
-    //         }
-    //     }
-    //     else
-    //     {
-    //         // Implement logic to set the device as a client e.g., connect to the server
-    //         Debug.Log("Client Mode");
-    //         if (isNetworkingEnabled)
-    //         {
-    //             Debug.Log("Networking Enabled. Setting up");
-    //             // SetClientMode();
-    //         }
-    //         else
-    //         {
-    //             Debug.Log("Networking is not enabled");
-    //         }
-    //     }
-
-    //     button_SetMode.GetComponent<Image>().color = isServer ? Color.red : Color.blue;
-    // } 
-
     public void ToggleClientServerMode()
     {
         isServer = !isServer; // Toggle server mode
@@ -393,10 +356,40 @@ public class u6_slider_ctrl : MonoBehaviour
         Start(); // Call the Start function to reset to initial values
     }
 
+    void Update()
+    {
+        if (isRecording)
+        {
+            RecordKeyframe();
+        }
+        if (!isRecording && keyframes.Count > 0)
+        {
+            //for windows
+            SaveKeyframesTo_txt_File();
+            //for android
+            SaveKeyframesToFile();
+        }
+        // server related code
+        if (!CheckIfServer())
+        {
+            // Implement client logic here
+            // Read slider changes and send them to the server
+            // Example: Send slider values to the server
+        }
+    }
+    // server related code
+    private bool CheckIfServer()
+    {
+        // Implement logic to check if the device should act as a server
+        // Return true for server, false for client
+        // Example: Use a tickle button or any other input method
+        return false; // Change this based on your logic
+    }
 
 
     public void AlterJoints()
     {
+        Debug.Log(isNetworkingEnabled);
         float rotationValue1 = slider1.value * 360f;
         float rotationValue2 = slider2.value * 360f;
         float rotationValue3 = slider3.value * 360f;
